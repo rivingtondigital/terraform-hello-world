6dc2d50e4f78addb997b06cb6093f07212831cd2
Jon <jon@rivingtondigital.com>
2019-08-30 17:37:05 +0700

    expandtab written on a blackboard a hundred times

--- variables.tf
+++ variables.tf
@@ -1,26 +1,27 @@
 # variables.tf
 
 variable "project_name"{
-	description	= "a regional jump off point"
-	default			= "origin_bastion"
+  description = "a regional jump off point"
+  default     = "origin_bastion"
 }
 
 variable "dest_region"{
-	description		= "the target endpoint"
-	default				= "us-west-1"
+  description = "the target endpoint"
+  default     = "us-west-1"
 }
 
 variable "origin_region"{
-	description		= "the region of origin"
-	default				= "ap-southeast-1"
+  description = "the region of origin"
+  default     = "ap-southeast-1"
 }
 
 variable "instance_type"{
-	description		= "how big"
-	default				= "t2.micro"
+  description = "how big"
+  default     = "t2.micro"
 }
 
 variable "key_name" {
-	description		= "key pair to use"
-	default			  = "singapore_micro"
+  description = "key pair to use"
+  default     = "singapore_micro"
 }
+
